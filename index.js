9de31c0e9aff1f1c237c9b85ed5cba6314d2a98b
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-03-09 19:10:00 +0300

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+// function reset() {
+//   cats.length = 0;
+//   cats.push("Milo", "Otis", "Garfield");
+// }
+beforeEach();
+cats.push("Ralph");
+cats.unshift("Bob");
+cats.splice(-1);
+cats.splice(1);
